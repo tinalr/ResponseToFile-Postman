3db3a5794481d887e7a062779fa6c2e370f40d15
Christina Rontell <christina-rontell@pluralsight.com>
2023-07-24 11:27:58 -0500

    added save to file script

--- script.js
+++ script.js
@@ -40,6 +40,6 @@ app.post('/write', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('ResponsesToFile App is listening now! Send them requests my way!');
+  console.log('ResponsesToFile App is listening now! Send them requests my way! http://localhost:3000');
   console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
 });
